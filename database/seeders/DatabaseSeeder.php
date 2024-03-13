45b7fdf3e2d24924b8e2ec345600c86352e271ee
José Rubén Ramírez Ruíz Velasco <40920586+JRubenRRV@users.noreply.github.com>
2024-03-13 13:19:13 -0600

    Seeder and softdeletes to users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Jose Ruben,Ramirez,Ruiz Velasco',
+            'email' => 'jruben.rrv95@gmail.com',
+            'password' => Hash::make('test1234'),
+            'created_at' => date('Y-m-d H:i:s')
+        ]);
     }
 }
